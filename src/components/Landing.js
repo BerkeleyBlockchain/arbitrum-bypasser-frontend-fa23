0cb7b02c82ff8d60b07d912bf0fb4c4a6b1d1d08
Arjun Patrawala <ajpat1234@gmail.com>
2023-11-29 17:50:36 -0800

    merge conflicts

---
+++ src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Landing.css';
+
+
+export default function Landing() {
+  return (
+    <div>
+      <div>Here is the landing page</div>
+      <a href={'/swap'}>Click here to go the swap page</a>
+    </div>
+  )
+}
