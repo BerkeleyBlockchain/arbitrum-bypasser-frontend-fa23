d7ce189d5347f5df55e2882e8eaec28a807d9d87
Tommy Hang <tommyhang@berkeley.edu>
2024-01-07 17:23:30 -0800

    change to arbiscan

--- src/utils/getTransactions.js
+++ src/utils/getTransactions.js
@@ -3,13 +3,16 @@ import axios from "axios";
 // const axios = require("axios");
 
 export async function getLastTransactions(account, num = 5) {
-  const apiKey = process.env.REACT_APP_ETHERSCAN;
   // change url if not using sepolia
-  const url = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${account}&startblock=0&endblock=99999999&page=1&offset=${num}&sort=desc&apikey=${apiKey}`;
+  // const apiKey = process.env.REACT_APP_ETHERSCAN;
+  // const url = `https://api-sepolia.etherscan.io/api?module=account&action=txlist&address=${account}&startblock=0&endblock=99999999&page=1&offset=${num}&sort=desc&apikey=${apiKey}`;
 
+  const apiKey = process.env.REACT_APP_ARBISCAN;
+  const url = `https://api-sepolia.arbiscan.io/api?module=account&action=txlist&address=${account}&startblock=0&endblock=99999999&page=1&offset=10&sort=desc&apikey=${apiKey}`;
   try {
     const response = await axios.get(url);
     const transactions = response.data.result;
+    console.log(transactions);
     return transactions.slice(0, num); // Return the last 5 transactions
   } catch (error) {
     console.error("Error fetching transactions:", error);
