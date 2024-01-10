ab6ddc8d74e233f652c2abc46c7bd8bd00f0b30e
JayTiptown <jaytipir@gmail.com>
2024-01-09 20:57:42 -0800

    added numerical input for gas slider

--- src/components/GasSlider.js
+++ src/components/GasSlider.js
@@ -1,16 +1,38 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
 import Slider from "@mui/material/Slider";
 
 export default function GasSlider({ gasState, setGasState }) {
+  const inputRef = React.useRef(null);
+
   const handleSliderChange = (event, newValue) => {
     setGasState(newValue);
   };
 
   const handleInputChange = (event) => {
-    setGasState(event.target.value === "" ? 0 : Number(event.target.value));
+    // Remove the percent sign and parse the remaining string as a floating-point number
+    const value = event.target.value.replace('%', '');
+    setGasState(value === "" ? '' : parseFloat(value));
+  };
+
+  const handleBlur = () => {
+    if (gasState < 0) {
+      setGasState(0);
+    } else if (gasState > 100) {
+      setGasState(100);
+    } else if (gasState === '') {
+      setGasState(0); // Reset to 0 if the input is empty
+    }
+  };
+
+  const handleInputFocus = () => {
+    const length = inputRef.current.value.length - 1; // Exclude the '%' sign
+    inputRef.current.setSelectionRange(length, length);
+  };
+
+  const handleInputClick = (e) => {
+    // Prevent the default input click behavior which moves the cursor
+    e.preventDefault();
+    handleInputFocus();
   };
 
   return (
@@ -25,9 +47,13 @@ export default function GasSlider({ gasState, setGasState }) {
         />
         <div className="w-12 text-left mr-2 ml-3">
           <input
+            ref={inputRef}
             type="text"
-            value={gasState + "%"}
+            value={gasState !== '' ? gasState + "%" : ''}
             onChange={handleInputChange}
+            onFocus={handleInputFocus}
+            onClick={handleInputClick}
+            onBlur={handleBlur}
             className="w-full p-2 text-white bg-gray-700 rounded focus:outline-none"
             style={{
               backgroundColor: "rgba(25, 29, 36, 1)",
