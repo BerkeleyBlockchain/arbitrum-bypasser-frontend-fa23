4005ff1d2d54c947cdc97ec6a53f63102f689c21
JayTiptown <jaytipir@gmail.com>
2024-01-22 00:13:00 -0800

    temp fix

--- src/containers/AddABI.js
+++ src/containers/AddABI.js
@@ -21,13 +21,8 @@ export default function AddABI() {
         body: formData,
       });
 
-      if (response.ok) {
-        // Redirect on successful submission
-        window.location.href = '/abi-confirmation';
-      } else {
-        // Handle errors or redirect to an error page
-        window.location.href = '/error-page';
-      }
+      window.location.href = '/abi-confirmation';
+      
     } catch (error) {
       console.error('Error submitting form:', error);
       window.location.href = '/error-page';
